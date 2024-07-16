db19619486b69afaf28f956a0c3e38250e9f5f70
David Wildasin <dragonmaus@posteo.net>
2024-07-16 00:17:00 -0400

    Re-indent build.gradle.kts
    Signed-off-by: David Wildasin <dragonmaus@posteo.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,36 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    `java-library`
-    `maven-publish`
+	kotlin("jvm") version "2.0.0"
+	`java-library`
+	`maven-publish`
 }
 
 group = "us.dragonma"
 version = "1.0.3"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation(platform(kotlin("bom")))
-    implementation(kotlin("stdlib-jdk8"))
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
+	implementation(platform(kotlin("bom")))
+	implementation(kotlin("stdlib"))
+	testImplementation(kotlin("test"))
+	testImplementation(kotlin("test-junit"))
 }
 
 java {
-    withSourcesJar()
+	withSourcesJar()
 }
 
 publishing {
-    repositories {
-        maven {
-            url = uri("${layout.buildDirectory}/repo")
-        }
-    }
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-    }
+	repositories {
+		maven {
+			url = uri("${layout.buildDirectory}/repo")
+		}
+	}
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+		}
+	}
 }
